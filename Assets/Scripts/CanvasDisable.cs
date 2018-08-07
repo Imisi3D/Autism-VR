5cfea2b71922629fa13eb6a2da361fc5d3b57414
Jehoshaphat I. Abu <jehoshaphatia@outlook.com>
2018-08-07 16:34:14 +0100

    Feat: First commit

---
+++ Assets/Scripts/CanvasDisable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasDisable : MonoBehaviour {
+
+	//public Text canvasText1;
+	public Canvas CanvasObject; // Assign in inspector
+
+	public float timeBeforeCanvasClose = 10f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("DisableText", timeBeforeCanvasClose);//invoke after 5 seconds
+
+	}
+	
+	void DisableText()
+	{ 
+		//canvasText1.enabled = false; 
+		CanvasObject.enabled = !CanvasObject.enabled;
+	} 
+}
---
+++ Assets/SpeechSandbox/Scripts/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+	public string levelOneToLoad;
+	public string levelTwoToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetDown (OVRInput.Button.SecondaryIndexTrigger)) 
+		{
+			//Load Male Scene
+			Debug.Log("Loading male scene");
+			SceneManager.LoadScene ("levelOneToLoad");
+		}
+		else if  (OVRInput.GetDown (OVRInput.Button.PrimaryIndexTrigger)) 
+		{
+			//Load Female Scene
+			Debug.Log("Loading Female scene");
+			SceneManager.LoadScene ("levelTwoToLoad");
+		}
+		
+	}
+}
